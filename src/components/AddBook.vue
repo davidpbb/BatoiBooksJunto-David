b9910bb95ddedbcc6e71eef50528ea5b6ec58c79
dei <davidpastorberenguer@gmail.com>
2025-01-29 21:33:04 +0100

    Vee-validate y yup integrado

--- src/components/AddBook.vue
+++ src/components/AddBook.vue
@@ -1,56 +1,62 @@
 <template>
 
-    <form ref="formulario">
+    <form>
 
         <br>
         <h4 id="titulo">Añadir Libro</h4>
         <label for="bookId">Id: </label>
-        <input id="bookId" v-model="newBookId" required /><br>
+        <input id="bookId" v-model="newBookId"/><br><br>
 
         <label for="bookModule">Módulo: </label>
         <select id="bookModule" v-model="newBookModule">
             <option disabled selected>-- SELECCIONA UN MÓDULO --</option>
             <option v-for="option in options" :value="option.code">{{ option.cliteral }}</option>
         </select><br>
+        <span v-if="moduleCodeError">{{ moduleCodeError }}</span><br><br>
 
         <label for="bookPublisher">Editorial: </label>
-        <input id="bookPublisher" v-model="newBookPublisher" required /><br>
+        <input id="bookPublisher" v-model="newBookPublisher" /><br>
+        <span v-if="publisherError">{{ publisherError }}</span><br><br>
 
         <label for="bookPrice">Precio: </label>
-        <input id="bookPrice" v-model="newBookPrice" required /><br>
+        <input id="bookPrice" v-model="newBookPrice" /><br>
+        <span v-if="priceError">{{ priceError }}</span><br><br>
 
         <label for="bookPages">Páginas: </label>
-        <input id="bookPages" v-model="newBookPages" required /><br>
+        <input id="bookPages" v-model="newBookPages" /><br>
+        <span v-if="pagesError">{{ pagesError }}</span><br><br>
 
-        <label>Estado: </label>
+        <label>Estado: </label><br>
 
-        <input type="radio" id="radioNew" name="bookStatus" value="new" v-model="newBookStatus" required />
+        <input type="radio" id="radioNew" name="bookStatus" value="new" v-model="newBookStatus" />
         <label for="radioNew">Nuevo</label><br>
 
-        <input type="radio" id="radioGood" name="bookStatus" value="good" v-model="newBookStatus" required />
+        <input type="radio" id="radioGood" name="bookStatus" value="good" v-model="newBookStatus" />
         <label for="radioGood">Bueno</label><br>
 
-        <input type="radio" id="radioUsed" name="bookStatus" value="used" v-model="newBookStatus" required />
+        <input type="radio" id="radioUsed" name="bookStatus" value="used" v-model="newBookStatus" />
         <label for="radioUsed">Usado</label><br>
 
-        <input type="radio" id="radioBad" name="bookStatus" value="bad" v-model="newBookStatus" required />
+        <input type="radio" id="radioBad" name="bookStatus" value="bad" v-model="newBookStatus" />
         <label for="radioBad">Malo</label><br>
+        <span v-if="statusError">{{ statusError }}</span><br><br>
 
-        <label for="comments">Comentarios: </label>
+        <label for="comments">Comentarios: </label><br>
         <textarea name="comments" id="comments" v-model="newBookComments"></textarea><br>
 
+        <!-- click.prevent llama a handleSubmit para comprobar que no haya errores de validación antes de enviar-->
         <div id="botones">
             <button 
                 type="submit"
                 id="editar"
                 v-if="editMode"
-                @click.prevent="editar"
+                @click.prevent="onSubmitEdit"
             >Editar</button>
             <button 
                 v-else
                 type="submit"
                 id="anyadir"
-                @click.prevent="addBook"
+                @click.prevent="onSubmitAdd"
             >Añadir</button>
             <button id="resetButton" @click="resetFields">Reset</button>
         </div>
@@ -65,20 +71,72 @@ import { ref, onMounted, nextTick, watch } from 'vue';
 import axios from 'axios';
 import { useStoreDeMensajes } from '../stores/messages';
 import { useRoute, useRouter } from 'vue-router';
+import { useForm, useField } from 'vee-validate';
+import * as yup from 'yup';
 
-//Campos reactivos del formulario
+
+//esquema de validación con yup
+const schema = {
+
+    moduleCode: yup.string()
+    .required('Código de módulo obligatorio'),
+
+    publisher: yup.string()
+    .required('La editorial es obligatoria'),
+
+    price: yup.number().
+    required('El precio es obligatorio')
+    .min(0, 'El precio no puede ser menor que 0')
+    .typeError('El campo debe ser un número')
+    .test('dos-decimales', 'El precio debe tener exactamente dos decimales', value => {
+      if (value === undefined || value === null) return false
+      return /^\d+\.\d{2}$/.test(value.toFixed(2))
+    }),
+
+    pages: yup.number()
+    .required('Las páginas son obligatorias')
+    .integer('Debe ser un numero entero')
+    .typeError('El campo debe ser un número')
+    .min(0, 'Las páginas no pueden ser menor que 0'),
+    
+    status: yup.string()
+    .required('El estado es obligatorio')
+}
+
+/* 
+* Desestructuración de useForm() que se le pasa el esquema como parametro y almacena en 
+* handleSubmit una función para manejar el envío del formulario con @submit.prevent.
+*/
+const { handleSubmit } = useForm({ validationSchema: schema });
+
+/*
+* useField(param) -> param es el nombre utilizado en el esquema para referirse al campo del moduleCode.
+* value contiene un campo ref() que se vincula con v-model en los inputs.
+* errorMessage contiene el error de validación para ese input específico
+* touched es una funcion devuelta por useField para comprobar si se ha hecho click
+* en el campo, para que se muestren los errores de forma responsive
+*/
+const { value: newBookModule, errorMessage: moduleCodeError, touched: touchedModule } = useField('moduleCode');
+const { value: newBookPublisher, errorMessage: publisherError, touched: touchedPublisher } = useField('publisher');
+const { value: newBookPrice, errorMessage: priceError, touched: touchedPrice } = useField('price');
+const { value: newBookPages, errorMessage: pagesError, touched: touchedPages } = useField('pages');
+const { value: newBookStatus, errorMessage: statusError, touched: touchedStatus } = useField('status');
+
+
+//Campos reactivos del formulario (comentados los campos ya que vee-validate ofrece campos con ref() en su propiedad 'value')
 const newBookId = ref("");
-const newBookModule = ref("");
-const newBookPublisher = ref("");
-const newBookPrice = ref("");
-const newBookPages = ref("");
-const newBookStatus = ref("");
 const newBookComments = ref("");
-const formulario = ref();
-
+// const newBookModule = ref("");
+// const newBookPublisher = ref("");
+// const newBookPrice = ref("");
+// const newBookPages = ref("");
+// const newBookStatus = ref("");
 
 //Array de modulos reactivo
 const options = ref([]);
+
+const onSubmitAdd = handleSubmit(addBook);
+const onSubmitEdit = handleSubmit(editar);
 
 //Store para mostrar mensajes
 const messages = useStoreDeMensajes();
@@ -90,10 +148,7 @@ let editMode = false;
 //Metodo que llama el botón del formulario si editmode = false;
 async function addBook() {
 
-    //Todos los campos del formulario no deben estar vacios
-    if (formulario.value.checkValidity()) {
-
-        try {
+    try {
 
             await axios.post('http://localhost:3000/books', {
             
@@ -118,22 +173,13 @@ async function addBook() {
         }
         //Redirige a la lista de libros
         router.push('/');
-
-    } else {
-
-        messages.addMessage('Todos los campos del formulario son obligatorios: ');
-        setTimeout(() => { messages.removeMessage(0) }, 3000);
-    }
     
 }
 
 //Metodo que llama el botón del formulario si editmode = true;
 async function editar() {
 
-    //Todos los campos del formulario no deben estar vacios
-    if (formulario.value.checkValidity()) {
-
-        try {
+    try {
 
             await axios.patch(`http://localhost:3000/books/${newBookId.value}`, {
 
@@ -158,12 +204,6 @@ async function editar() {
 
         //Redirige a la lista de libros
         router.push('/');
-
-    } else {
-
-        messages.addMessage('Todos los campos del formulario son obligatorios: ');
-        setTimeout(() => { messages.removeMessage(0) }, 3000);
-    }
 
 }
 
